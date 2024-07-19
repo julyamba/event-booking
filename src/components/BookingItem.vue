3ed4bebd4759da55b9867c45382d4f76c492f278
julyamba <jyamz7690@gmail.com>
2024-07-19 09:30:29 +0800

    progress update

--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-between">
       <div class="flex space-x-2">
         <div>{{ title }}</div>
-        <div>{{ status }}</div>
+        <div><component :is="icon" :class="{ 'animate-spin': pending }" :color="color" /></div>
       </div>
       <RoundedButton variant="danger" @click="$emit('cancelled')">Cancel</RoundedButton>
     </div>
@@ -13,9 +13,14 @@
 <script setup>
 import SectionCard from '@/components/SectionCard.vue';
 import RoundedButton from '@/components/RoundedButton.vue';
-defineProps({
+import { LoaderCircle, Check } from 'lucide-vue-next';
+import { computed } from 'vue';
+const props = defineProps({
   title: String,
   status: String
 });
+const pending = computed(() => props.status === 'pending');
+const icon = computed(() => (pending.value ? LoaderCircle : Check));
+const color = computed(() => (pending.value ? '#a6a6a6' : '#4eb558'));
 defineEmits(['cancelled']);
 </script>
